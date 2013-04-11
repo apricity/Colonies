daea468175f5325584ddced0c15c622636c4e78b
wacton <apricity.lxv@gmail.com>
2013-04-11 17:40:52 +0100

    Habitat condition comments

--- Colonies/Models/HabitatCondition.cs
+++ Colonies/Models/HabitatCondition.cs
@@ -2,6 +2,9 @@
 {
     public class HabitatCondition
     {
+        // TODO: each type of strength (e.g. pheromone, heat, moisture) should be between 0.0 - 1.0
+        // TODO: the organism will assign a multiplier to that value (could change according to organism type / genetics)
+        // TODO: that value will be added to the weighting of that habitat
         public readonly int Strength;
 
         public HabitatCondition(int strength)

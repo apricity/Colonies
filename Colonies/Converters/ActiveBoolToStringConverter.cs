2f2ae08b420168d6383e262384528643ebe0544c
wacton <apricity.lxv@gmail.com>
2013-04-21 16:08:47 +0100

    Neater UI controls + basic temporary icon

---
+++ Colonies/Converters/ActiveBoolToStringConverter.cs
@@ -0,0 +1,39 @@
+﻿namespace Colonies.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+    using System.Windows.Media;
+
+    public class ActiveBoolToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+
+            if (value is bool)
+            {
+                var active = (bool)value;
+                if (active)
+                {
+                    return "Active";
+                }
+                else
+                {
+                    return "Inactive";
+                }
+            }
+
+            Type type = value.GetType();
+            throw new InvalidOperationException("Unsupported type [" + type.Name + "]"); 
+        }
+
+        public object ConvertBack(object value, Type targetType,object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

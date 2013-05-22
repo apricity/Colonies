de3b67cd91c034f58d5db918012ed49c2e7805f7
wacton <apricity.lxv@gmail.com>
2013-05-22 14:22:29 +0100

    Decision tests independent from domain objects

--- Colonies.Tests/RandomDecisionTests.cs
+++ Colonies.Tests/RandomDecisionTests.cs
@@ -14,74 +14,63 @@
     [TestFixture]
     public class RandomDecisionTests
     {
-        private Dictionary<string, Organism> organisms;
+        private List<string> items;
 
         [SetUp]
         public void SetupTest()
         {
-            var organismIdentifiers = new List<string> { "A", "B", "C", "D", "W", "X", "Y", "Z" };
-
-            this.organisms = new Dictionary<string, Organism>();
-            foreach (var organismIdentifier in organismIdentifiers)
-            {
-                this.organisms.Add(organismIdentifier, new Organism(organismIdentifier, Color.Black, false));
-            }
+            this.items = new List<string> { "A", "B", "C", "D", "W", "X", "Y", "Z" };
         }
 
         [Test]
-        public void EqualOrganisms()
+        public void EqualItems()
         {
-            // all organisms will have the same measurements by default
-            // (currently, health = 1.0)
-            var organismMeasurements = this.organisms.Values.ToDictionary(
-                organism => organism,
-                organism => organism.GetMeasurement());
+            // all items will only have one measurement, and they will all be the same
+            var measurement = new Measurement(new List<Condition> { new Condition(Measure.Pheromone, 1.0) });
+            var itemMeasurements = this.items.ToDictionary(item => item, item => measurement);
 
-            // there needs to be a bias for each organism measure, and has to be the same for all organisms
-            // (this will have no effect when there is only one measure)
-            var measureBiases = new Dictionary<Measure, double> { { Measure.Health, 1.0 } };
+            // there needs to be a bias for each measure (will be applied to all items)
+            // but will have no effect when there is only one measure
+            var measureBiases = new Dictionary<Measure, double> { { Measure.Pheromone, 1.0 } };
 
             // override the random number generator used by the decision logic so it can be manipulated
             var mockRandom = new MockRandom();
             DecisionLogic.SetRandomNumberGenerator(mockRandom);
 
-            var chosenOrganisms = new List<Organism>();
+            var chosenItems = new List<string>();
             
             // the numbers generated are distributed evenly based on the number of organisms
             // therefore there should be each organism should be chosen once by the decision logic
-            var increment = 1.0 / this.organisms.Count;
+            var increment = 1.0 / this.items.Count;
             for (var nextDouble = 0.0; nextDouble < 1.0; nextDouble += increment)
             {
                 mockRandom.SetNextDouble(nextDouble);
-                chosenOrganisms.Add(DecisionLogic.MakeDecision(organismMeasurements, measureBiases));
+                chosenItems.Add(DecisionLogic.MakeDecision(itemMeasurements, measureBiases));
             }
 
             // expecting each organism to be chosen once, so expecting the same as the original list
-            var expectedOrganisms = this.organisms.Values.ToList();
-            var actualOrganisms = chosenOrganisms;
-            Assert.That(actualOrganisms, Is.EqualTo(expectedOrganisms));
+            var expectedItems = this.items;
+            var actualItems = chosenItems;
+            Assert.That(actualItems, Is.EqualTo(expectedItems));
         }
 
         [Test]
-        public void InequalOrganisms()
+        public void InequalItems()
         {
-            // all organisms will have the same measurements by default
-            // (currently, health = 1.0)
-            var organismMeasurements = this.organisms.Values.ToDictionary(
-                organism => organism,
-                organism => organism.GetMeasurement());
-
-            // set organism health levels to change the frequency with which they are chosen
-            var decrement = 1.0 / this.organisms.Count;
-            for (var i = 0; i < this.organisms.Count; i++)
+            // all items will only have one measurement but the levels will all be different
+            // by an even spread based on how many items there are
+            var itemMeasurements = new Dictionary<string, Measurement>();
+            var measurementLevelChange = 1.0 / this.items.Count;
+            for (var i = 0; i < this.items.Count; i++)
             {
-                var healthDecrement = i * decrement;
-                organismMeasurements.ElementAt(i).Key.DecreaseHealth(healthDecrement);
+                var measurementLevel = (i + 1) * measurementLevelChange;
+                var measurement = new Measurement(new List<Condition> { new Condition(Measure.Pheromone, measurementLevel) });
+                itemMeasurements.Add(this.items.ElementAt(i), measurement);
             }
 
-            // there needs to be a bias for each organism measure, and has to be the same for all organisms
-            // (this will have no effect when there is only one measure)
-            var measureBiases = new Dictionary<Measure, double> { { Measure.Health, 1.0 } };
+            // there needs to be a bias for each measure (will be applied to all items)
+            // but will have no effect when there is only one measure
+            var measureBiases = new Dictionary<Measure, double> { { Measure.Pheromone, 1.0 } };
 
             // override the random number generator used by the decision logic so it can be manipulated
             // and set the base weighting to 0 (so that chance of being chosen is based directly on measurment level * bias)
@@ -89,35 +78,41 @@
             DecisionLogic.SetRandomNumberGenerator(mockRandom);
             DecisionLogic.SetBaseWeighting(0.0);
 
-            var chosenOrganisms = new List<Organism>();
+            var chosenItems = new List<string>();
 
-            // the numbers generated need to reflect the range of health levels in the organisms
-            // if there are 8 organisms...
-            // - 8/8 health -> 8 results, 7/8 health -> 7 results, 6/8 health -> 6 results, ..., 1/8 health -> 1 result
+            // the numbers generated need to reflect the range of measurement levels in the items
+            // if there are 8 items...
+            // - 8/8 pheromone -> 8 results
+            // - 7/8 pheromone -> 7 results
+            // - 6/8 pheromone -> 6 results
+            // - ...
+            // - 1/8 pheromone -> 1 result
             // total number of results: 1 + 2 + 3 + 4 + ... = n(n + 1)/2 [sum of integers from 1 - n]
-            var numberOfResults = this.organisms.Count() * (this.organisms.Count() + 1) / 2;
+            var numberOfResults = this.items.Count() * (this.items.Count() + 1) / 2;
             var increment = 1.0 / numberOfResults;
             for (var nextDouble = 0.0; nextDouble < 1.0; nextDouble += increment)
             {
                 mockRandom.SetNextDouble(nextDouble);
-                chosenOrganisms.Add(DecisionLogic.MakeDecision(organismMeasurements, measureBiases));
+                chosenItems.Add(DecisionLogic.MakeDecision(itemMeasurements, measureBiases));
             }
 
             // expecting each organism to be chosen a number of times proportional to their health
-            var expectedOrganismCounts = new Dictionary<Organism, int>();
-            for (var i = 0; i < this.organisms.Count; i++)
+            var expectedItemCounts = new Dictionary<string, int>();
+            for (var i = 0; i < this.items.Count; i++)
             {
-                expectedOrganismCounts.Add(this.organisms.ElementAt(i).Value, this.organisms.Count - i);
+                expectedItemCounts.Add(this.items.ElementAt(i), i + 1);
             }
 
-            var actualOrganismCounts = new Dictionary<Organism, int>();
-            foreach (var organism in this.organisms.Values)
+            var actualItemCounts = new Dictionary<string, int>();
+            foreach (var item in this.items)
             {
-                var numberOfTimesChosen = chosenOrganisms.Count(chosenOrganism => chosenOrganism.Equals(organism));
-                actualOrganismCounts.Add(organism, numberOfTimesChosen);
+                var numberOfTimesChosen = chosenItems.Count(chosenItem => chosenItem.Equals(item));
+                actualItemCounts.Add(item, numberOfTimesChosen);
             }
 
-            Assert.That(actualOrganismCounts, Is.EqualTo(expectedOrganismCounts));
+            Assert.That(actualItemCounts, Is.EqualTo(expectedItemCounts));
         }
     }
 }
+
+

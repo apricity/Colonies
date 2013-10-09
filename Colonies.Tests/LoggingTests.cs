bc4743c7861a72686cccac633a8586fc22870dcb
wacton <apricity.lxv@gmail.com>
2013-10-09 12:55:51 +0100

    Logging outline + example test

---
+++ Colonies.Tests/LoggingTests.cs
@@ -0,0 +1,24 @@
+﻿namespace Wacton.Colonies.Tests
+{
+    using NUnit.Framework;
+
+    using Wacton.Colonies.Logging;
+
+    [TestFixture]
+    public class LoggingTests
+    {
+        [Test]
+        public void SettingFlagChangesLogger()
+        {
+            Assert.IsFalse(Logger.GetFlag());
+
+            Logger.SetFlag(true);
+            Assert.IsTrue(Logger.GetFlag());
+
+            Logger.SetFlag(false);
+            Assert.IsFalse(Logger.GetFlag());
+        }
+    }
+}
+
+

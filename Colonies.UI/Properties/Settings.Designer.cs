87ee2de597b7e86a5b3dc0921deaf803220aad59
wacton <apricity.lxv@gmail.com>
2015-08-31 10:07:53 +0100

    Revert "Revert "MainViewModel naming updates""
    This reverts commit d7be969e6472256d3aa29596aab625e8ac6683c9.

--- Colonies.UI/Properties/Settings.Designer.cs
+++ Colonies.UI/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Wacton.Colonies.UI.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,12 +26,12 @@ namespace Wacton.Colonies.UI.Properties {
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("50")]
-        public int PhaseIntervalInMs {
+        public int PhaseTimerIntervalInMs {
             get {
-                return ((int)(this["PhaseIntervalInMs"]));
+                return ((int)(this["PhaseTimerIntervalInMs"]));
             }
             set {
-                this["PhaseIntervalInMs"] = value;
+                this["PhaseTimerIntervalInMs"] = value;
             }
         }
     }
